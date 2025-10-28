a99037168acfcf8e95c1606ce403e2d1da2b2677
Mahima-patidar441 <mahimapatidar776@gmail.com>
2025-10-29 00:10:46 +0530

    Add stoi and stoll examples in stoi_stoll.cpp

---
+++ DSA/String/stoi_stoll.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str = "32543";
+    int x = stoi(str);
+    cout<<x+1<<endl;
+
+    string str2 = "3565767633";
+    long long y = stoll(str2);
+    cout<<y<<endl;
+
+}
