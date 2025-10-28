fcbb4fc1450f9ff1a6a9902bf8638ab87ffba792
Mahima-patidar441 <mahimapatidar776@gmail.com>
2025-10-28 23:33:52 +0530

    Create Reverse_array.cpp

---
+++ DSA/Array1D/Reverse_array.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v1; 
+    v1.push_back(4);
+    v1.push_back(7);
+    v1.push_back(8);
+    v1.push_back(9);
+    v1.push_back(3);
+    v1.push_back(1);
+    
+    for(int i=0; i<v1.size() ; i++){
+        cout<<v1[i]<<" ";
+    }
+    cout<<endl;
+    int i = 0 ;
+    int j = v1.size()-1;
+    while(i<=j){
+        int temp = v1[i];
+        v1[i] = v1[j];
+        v1[j] = temp ;
+        i++;
+        j--;
+    }
+
+    for(int i=0; i<v1.size() ; i++){
+        cout<<v1[i]<<" ";
+    }
+
+    //inbuilt function --> reverse(v.begin(),v.end());
+}
