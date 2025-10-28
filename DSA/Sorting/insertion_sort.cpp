cf625640787571ded24f3d9869fd47214422bb94
Mahima-patidar441 <mahimapatidar776@gmail.com>
2025-10-28 23:56:35 +0530

    Create insertion_sort.cpp

---
+++ DSA/Sorting/insertion_sort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5] = {5, 3, 4, 2, 1};
+    int n = 5;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    for (int i = 1; i < n; i++)
+    {
+        int j = i;
+        while (j >= 1 && arr[j] < arr[j - 1])
+        {
+            swap(arr[j], arr[j - 1]);
+            j--;
+        }
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
