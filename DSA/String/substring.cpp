b9eda72f65e47a7f1d3983b958d3b41cc24fa7c1
Mahima-patidar441 <mahimapatidar776@gmail.com>
2025-10-29 00:04:28 +0530

    Create substring.cpp

---
+++ DSA/String/substring.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    string s = "Mahima";
+    cout<<s.substr(2)<<endl;
+    cout<<s.substr(1,4)<<endl;
+  
+}
