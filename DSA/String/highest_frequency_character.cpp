1df288d693886f4918aba9064b72ddc94600bde4
Mahima-patidar441 <mahimapatidar776@gmail.com>
2025-10-29 00:08:37 +0530

    Add highest frequency character finder
    Implement a program to find the highest frequency character in a string.

---
+++ DSA/String/highest_frequency_character.cpp
@@ -0,0 +1,45 @@
+//not a good way
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string s;
+    cout << "Enter a string:";
+    cin >> s;
+    int max = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        char ch = s[i];
+        int count = 1;
+        for (int j = i + 1; j < s.length(); j++)
+        {
+            if (s[i] == s[j])
+            {
+                count++;
+            }
+        }
+        if (max < count)
+        {
+            max = count;
+        }
+    }
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        char ch = s[i];
+        int count = 1;
+        for (int j = i + 1; j < s.length(); j++)
+        {
+            if (s[i] == s[j])
+            {
+                count++;
+            }
+        }
+        if (count == max)
+        {
+            cout << ch << " " << max << endl;
+        }
+    }
+}
