b91b82447c5a9cc07f6110acb9bdb7f3a55dec83
Mahima-patidar441 <mahimapatidar776@gmail.com>
2025-11-06 20:37:47 +0530

    Create Tower_Of_Hanoi.cpp

---
+++ DSA/Recursion/Tower_Of_Hanoi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void Hanoi(int n, char a, char b, char c)
+{
+    if(n==0) return ;
+    Hanoi(n - 1, a, c, b);
+    cout << a << " --> " << c << endl;
+    Hanoi(n - 1, b, a, c);
+}
+int main()
+{
+    int n = 4;
+    Hanoi(n, 'A', 'B', 'C');
+}
