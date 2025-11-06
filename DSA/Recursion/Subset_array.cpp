fe98ad8eabd5e2adc67e9659c969d051eaebddc5
Mahima-patidar441 <mahimapatidar776@gmail.com>
2025-11-06 20:36:59 +0530

    Create Subset_array.cpp

---
+++ DSA/Recursion/Subset_array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void printSubsets(int arr[], int n, int idx, vector<int> ans)
+{
+    if(idx ==n ){
+        for(int i = 0; i <ans.size() ; i++){
+            cout<<ans[i]<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    printSubsets(arr, n, idx + 1, ans);
+    ans.push_back(arr[idx]);
+    printSubsets(arr, n, idx + 1, ans);
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    vector<int> v;
+    printSubsets(arr, n, 0, v);
+} 
